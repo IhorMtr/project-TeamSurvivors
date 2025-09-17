1dc7da5cad1b70fe1e6e66eb9631e02c2fd4353d
IhorMtr <igormotornyyy@gmail.com>
2025-09-17 18:59:20 +0300

    added cors exception

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,7 @@
 import path from 'node:path';
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
-export const allowedOrigins = ['http://localhost:3000'];
+export const allowedOrigins = [
+  'http://localhost:3000',
+  'https://project-team-survivors-front.vercel.app/',
+];
