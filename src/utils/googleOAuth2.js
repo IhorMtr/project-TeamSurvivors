44d39e870f781a5ab4dc25d9d80fdaef9c7135f5
IhorMtr <igormotornyyy@gmail.com>
2025-09-29 17:44:01 +0300

    added googleOAuth

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -1,17 +1,14 @@
 import { OAuth2Client } from 'google-auth-library';
-import path from 'node:path';
-import { readFile } from 'fs/promises';
 import createHttpError from 'http-errors';
 import { getEnvVar } from './getEnvVar.js';
-
-const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
-
-const oauthConfig = JSON.parse(await readFile(PATH_JSON));
 
 const googleOAuthClient = new OAuth2Client({
   clientId: getEnvVar('GOOGLE_AUTH_CLIENT_ID'),
   clientSecret: getEnvVar('GOOGLE_AUTH_CLIENT_SECRET'),
-  redirectUri: oauthConfig.web.redirect_uris[0],
+  redirectUri:
+    process.env.NODE_ENV === 'production'
+      ? getEnvVar('GOOGLE_AUTH_REDIRECT_URI_PROD')
+      : getEnvVar('GOOGLE_AUTH_REDIRECT_URI_LOCAL'),
 });
 
 export const generateAuthUrl = () =>
